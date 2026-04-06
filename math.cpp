119c17eedd3556ccb2dfbf5eff24e9487989eda8
Vaibhav Chavan <chavanvaibhav678@gmail.com>
2026-04-06 09:58:10 +0530

    Added factorial function

---
+++ math.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+// Vaibhav: Factorial
+int factorial(int n) {
+    int fact = 1;
+    for(int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
+}
